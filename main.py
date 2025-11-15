f9d96bcca131c07dff5982e2a0ce83e2accf55ca
olyasha4U <lybo.km15@gmail.com>
2025-11-15 15:07:23 +0200

    init

--- main.py
+++ main.py
@@ -11,17 +11,32 @@
 #     print(next(a))
 # except:
 #     print("В списку закінчились слова")
-2
-class Number:
-    def __next__(self):
-        self.a=2
+# 2
+# class Number:
+#     def __init__(self):
+#         self.a=2
+#     def __iter__(self):
+#         self.a=2
+#         print(self.a)
+#         return self
+#     def __next__(self):
+#         self.a *=2
+#         return self.a
+# n=iter(Number())
+# for i in range (10):
+#     print(next(n))
+
+3
+class Number_1:
+    def __init__(self):
+        self.b=3
     def __iter__(self):
-        self.a=2
-        print(self.a)
+        self.b=3
+        print(self.b)
         return self
     def __next__(self):
-        self.a *=2
-        return self.a
-n=iter(Number())
-for i in range (10):
-    print(next(n))
+        self.b *=3
+        return self.b
+n1=iter(Number_1())
+for i in range(10):
+    print(next(n1))
