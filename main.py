8aa259ca6e552cb60f0bb414cb8aafeae05affb7
olyasha4U <lybo.km15@gmail.com>
2025-11-15 14:58:55 +0200

    init

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 
 # 1
-words = ["Cat", "Dog", "Rabbit", "Fish"]
-lengths = [len(w) for w in words]
-a = iter(lengths)
-print(next(a))
-print(next(a))
-print(next(a))
-print(next(a))
-try:
-    print(next(a))
-except:
-    print("В списку закінчились слова")
+# words = ["Cat", "Dog", "Rabbit", "Fish"]
+# lengths = [len(w) for w in words]
+# a = iter(lengths)
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# try:
+#     print(next(a))
+# except:
+#     print("В списку закінчились слова")
+2
+class Number:
+    def __next__(self):
+        self.a=2
+    def __iter__(self):
+        self.a=2
+        print(self.a)
+        return self
+    def __next__(self):
+        self.a *=2
+        return self.a
+n=iter(Number())
+for i in range (10):
+    print(next(n))
